b596ea8b6c5341a9cb72d1651b124fe4bb37cf91
lucassmun <lucassmun@gmail.com>
2022-09-27 18:54:06 -0300

    listas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListasComponent } from './components/listas/listas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListasComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/listas/listas.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Persona } from 'src/app/models/persona';
+
+@Component({
+  selector: 'app-listas',
+  templateUrl: './listas.component.html',
+  styleUrls: ['./listas.component.css']
+})
+export class ListasComponent implements OnInit {
+
+  listaNombres: Array<Persona> = [
+    {nombre: 'Diego', edad: 20, nota: 8},
+    {nombre: 'Gregorio', edad: 26, nota: 4},
+    {nombre: 'Luciano', edad: 21, nota: 2},
+    {nombre: 'Pamela', edad: 33, nota: 10},
+    {nombre: 'Eugenia', edad: 18, nota:3},
+
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
